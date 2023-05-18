b7b9f1c4d0a817bb0081c89f1fda1250d18dc30c
licon4812 <jalickolli@gmail.com>
2023-05-18 23:30:38 +1000

    updated namespaces, metadata and dependencies

---
+++ src/main/java/com/licon4812/naruto_sp_addon/NarutoSPAddon.java
@@ -0,0 +1,31 @@
+package com.licon4812.naruto_sp_addon;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = NarutoSPAddon.MODID, name = NarutoSPAddon.NAME, version = NarutoSPAddon.VERSION)
+public class NarutoSPAddon
+{
+    public static final String MODID = "naruto_sp_addon";
+    public static final String NAME = "Naruto SP Addon";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
